4af05623edd9191492bdd118237ef7b537e45efd
Harshdeep Singh <hdsingharora99@gmail.com>
2021-03-11 03:16:58 +0530

    delete button fixed

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,34 @@
-const deleteProduct = (btn) => {
+// const deleteProduct = (btn) => {
+//     console.log('Clicked');
+//     console.log(btn);
+//     const prodId = btn.parentNode.querySelector('[name=productId]').value; //gives product id
+//     const csrf = btn.parentNode.querySelector('[name=_csrf]').value; //gives csrf value
+//     const productElement = btn.closest('article');
+
+
+//     fetch('/admin/product/' + prodId, { //here we can configure request
+//         method: 'DELETE',
+//         headers: {
+//             'csrf-token':csrf //csurf handler will automatically look for this
+//         }
+//     }).then(result => {
+//         //console.log(result);
+//         return result.json();
+//     })
+//         .then(data => {
+//             //console.log(data);
+//             productElement.remove();
+//         })
+//         .catch(err => {
+//             console.log(err);
+//         })
+// };
+
+const btn = document.querySelector('[id=delete-btn]');
+btn.addEventListener('click', () => {
     console.log('Clicked');
-    console.log(btn);
-    const prodId = btn.parentNode.querySelector('[name=productId]').value; //gives product id
+
+    const prodId =btn.parentNode.querySelector('[name=productId]').value; //gives product id
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value; //gives csrf value
     const productElement = btn.closest('article');
 
@@ -9,7 +36,7 @@ const deleteProduct = (btn) => {
     fetch('/admin/product/' + prodId, { //here we can configure request
         method: 'DELETE',
         headers: {
-            'csrf-token':csrf //csurf handler will automatically look for this
+            'csrf-token': csrf //csurf handler will automatically look for this
         }
     }).then(result => {
         //console.log(result);
@@ -22,4 +49,4 @@ const deleteProduct = (btn) => {
         .catch(err => {
             console.log(err);
         })
-};
+});
